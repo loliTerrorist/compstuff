b1a1945b94004210a5345b0ef7fed6a9dcbc9e55
LoliTerrorist <theclumsypenguinlol@gmail.com>
2022-11-12 15:23:01 +0800

    added files

---
+++ cses/Digit_Queries.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define pb push_back
+using namespace std;
+//1~9, 10~99, 100~999, 1000~9999, 10^n~10^(n+1) - 1
+int main(){
+    int q;
+    while(q--){
+        ll k; cin>>k;
+        int tenpow = 0;
+        
+    }
+}
--- cses/chessboard_and_queens.cpp
+++ cses/chessboard_and_queens.cpp
@@ -4,23 +4,29 @@
 #define endl "\n"
 using namespace std;
 int ans=0;
-vector<bool> sum(15), dif(15), c(8);
+// if not available 1
+vector<bool> sum(15,0), dif(15,0), c(8,0);
+vector<string> vs(8);
 void search(int r){
     if(r==8){
         ans++;
         return;
     }else{
         for(int i = 0; i<8;i++){
-
+            if(vs[r][i]=='.'&&!sum[r+i]&&!dif[(r-i)+7]&&!c[i]){
+                sum[r+i]=dif[7+(r-i)]=c[i]=1;
+                search(r+1);
+                sum[r+i]=dif[7+(r-i)]=c[i]=0;
+            }
         }
     }
     
 }
 
 int main(){
-    vector<string> vs(8);
     for(int i = 0; i<8;i++){
         cin>>vs[i];
     }
-
+    search(0);
+    cout<<ans<<endl;
 }
